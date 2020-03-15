e0a22e6f8de8e4357e323d96a5252d182b656586
Xuyang Kang <xuyangkang@gmail.com>
2020-03-16 08:47:33 +0900

    Hello world API

--- django-rest/testproject/api/views.py
+++ django-rest/testproject/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class Secret(APIView):
+    def get(self, request, format=None):
+        return Response({'secret': 'shrrrr..'})
+
+    def post(self, request, format=None):
+        return Response({'secret': 'shrrrr..'})
+
+
+class NotSecret(APIView):
+    def get(self, request, format=None):
+        return Response({'public': 'public'})
+
+    def post(self, request, format=None):
+        return Response({'public': 'public'})
