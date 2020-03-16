1d7625b7690f13f77f1b622cc95e28a3326740da
Xuyang Kang <xuyangkang@gmail.com>
2020-03-16 09:37:38 +0900

    Guard secret class with auth

--- django-rest/testproject/api/views.py
+++ django-rest/testproject/api/views.py
@@ -2,14 +2,25 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 
 class Secret(APIView):
+    authentication_classes = [TokenAuthentication]
+
     def get(self, request, format=None):
-        return Response({'secret': 'shrrrr..'})
+        if request.user.is_authenticated:
+            print(request.user)
+            return Response({'secret': 'this is a secret'})
+        else:
+            return Response({'secret': 'shrrrr..'})
 
     def post(self, request, format=None):
-        return Response({'secret': 'shrrrr..'})
+        if request.user.is_authenticated:
+            print(request.user)
+            return Response({'secret': 'this is a secret'})
+        else:
+            return Response({'secret': 'shrrrr..'})
 
 
 class NotSecret(APIView):
