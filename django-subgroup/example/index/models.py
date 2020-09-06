8f7328f52820a0004fb2a595690700995ed29fa7
Xuyang Kang <xuyang.kang@exwzd.com>
2020-09-07 05:53:26 +0900

    Test logics

--- django-subgroup/example/index/models.py
+++ django-subgroup/example/index/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+
+UserModel = get_user_model()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        UserModel,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    memo = models.TextField(blank=True, null=True)
+
+
+class GroupProfile(models.Model):
+    group = models.OneToOneField(
+        Group,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    parent_group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='parent_group_to_group')
+    name = models.TextField(blank=True, null=True)
+
+
+class Comapny(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
